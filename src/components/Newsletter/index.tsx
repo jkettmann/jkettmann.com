047831cd650e061bc5dfeab496b5b9de40fcd000
Johannes Kettmann <jkettmann@gmx.net>
2020-12-17 08:48:03 +0100

    Add labels to newsletter inputs

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -7,11 +7,25 @@ type MailerliteBoxProps = {
 
 const MailerliteBox = React.memo(({ formId }: MailerliteBoxProps) => {
   useEffect(() => {
+    const form = document.getElementById('subscribe-form');
+
+    const callback = function(mutationList, observer) {
+      const inputs = document.getElementById('subscribe-form').getElementsByTagName('input');
+      inputs[0].setAttribute('aria-label', 'Name');
+      inputs[1].setAttribute('aria-label', 'Email');
+      observer.disconnect();
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(form, { childList: true });
+
     window.MailerLiteObject = 'ml';
     const script = document.createElement('script');
     script.async = true;
     script.src = 'https://static.mailerlite.com/js/universal.js?v'+(~~(new Date().getTime()/1000000));
-    script.onload = () => window.ml('accounts', '1382888', 'c4v2p8y4b0', 'load');
+    script.onload = () => {
+      window.ml('accounts', '1382888', 'c4v2p8y4b0', 'load');
+    }
     document.head.appendChild(script);
   }, []);
 
