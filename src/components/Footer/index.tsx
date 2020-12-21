9d5ed63100d463e94047370d33ecad5774ef4f8c
Johannes Kettmann <jkettmann@gmx.net>
2020-12-21 21:20:00 +0100

    Add social icons and legal page to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
-
+import LinkedInIcon from '../Icons/linkedin.svg';
+import TwitterIcon from '../Icons/twitter.svg';
+import GithubIcon from '../Icons/github.svg';
+import EmailIcon from '../Icons/email.svg';
 import * as S from './styles';
 
 const Footer: React.FC = () => (
   <S.Footer>
-    <S.Link
-      href="https://github.com/jkettmann"
-      rel="noreferrer noopener"
-      target="_blank"
-    >
-      GitHub
-    </S.Link>
-    <S.Link
-      href="https://twitter.com/j_kettmann"
-      rel="noreferrer noopener"
-      target="_blank"
-    >
-      Twitter
-    </S.Link>
+    <S.Container>
+      <S.Placeholder>
+        Legal
+      </S.Placeholder>
+      <S.IconWrapper>
+        <S.IconLink
+          href="https://github.com/jkettmann"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <S.Icon src={GithubIcon} />
+        </S.IconLink>
+        <S.IconLink
+          href="https://twitter.com/j_kettmann"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <S.Icon src={TwitterIcon} />
+        </S.IconLink>
+        <S.IconLink
+          href="https://www.linkedin.com/in/johannes-kettmann-40a049145/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <S.Icon src={LinkedInIcon} />
+        </S.IconLink>
+        <S.IconLink
+          href="mailto:hi@jkettmann.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <S.Icon src={EmailIcon} />
+        </S.IconLink>
+      </S.IconWrapper>
+
+      <S.LegalLink to="/legal">
+        Legal
+      </S.LegalLink>
+    </S.Container>
   </S.Footer>
 );
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,13 +1,35 @@
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
+import UnstyledContainer from 'components/ui/Container';
 
 export const Footer = styled.footer`
-  ${tw`flex items-center justify-center w-full border-t border-gray-200`};
+  ${tw`flex border-t border-gray-200`};
   height: 70px;
 `;
 
-export const Link = styled.a`
-  ${tw`mx-2`};
-  color: #2a233d;
+export const Container = styled(UnstyledContainer)`
+  ${tw`items-center justify-between`};
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+`;
+
+export const IconLink = styled.a`
+  margin: 0 8px;
+  text-decoration: none;
+  border: none;
+`;
+
+export const Icon = styled.img`
+
+`;
+
+export const Placeholder = styled.div`
+  visibility: hidden;
+`;
+
+export const LegalLink = styled(Link)`
   border: none;
 `;
