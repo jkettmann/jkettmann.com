2384741a2880de90c2368f859bee91f731ef3ed5
Johannes Kettmann <jkettmann@gmx.net>
2020-12-23 07:39:07 +0100

    Fix RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,60 @@ module.exports = {
       }
     },
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: `${site.siteMetadata.siteUrl}/${edge.node.frontmatter.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/${edge.node.frontmatter.slug}`,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: { frontmatter: { category: { eq: "blog" } } }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "jkettmann.com RSS Feed",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-webpack-bundle-analyser-v2`
   ]
 };
